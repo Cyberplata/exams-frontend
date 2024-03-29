1f37fa334a93309bd2ab0ea8f4e37cbf2b7c9c4c
PlatonKynin <kininplaton@gmail.com>
2024-03-29 21:39:17 +0300

    01sprint-1week-5task

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,26 @@
 import ReactDOM from 'react-dom'
 
-export const VideoHeader = (props: {videoName: string}) => {
-    return <div>
-        😀 {props.videoName}
-    </div>
-}
-export const VideoContent = (props: {videoContent: string}) => {
-    return <div>
-        📼 <a href={props.videoContent}>{props.videoContent}</a>
-    </div>
-}
-export const VideoDescription = (props: {videoDescription: string}) => {
-    return <div>
-        📑 {props.videoDescription}
-    </div>
-}
+const CrazyButton = (props: any) => {
 
-export const YoutubeVideo = (props: any) => {
-    return <div>
-        <VideoHeader videoName={props.title} />
-        <VideoContent videoContent={props.link} />
-        <VideoDescription videoDescription={props.description} />
-    </div>
+    const style = {
+        color: props.fontColor,
+        backgroundColor: props.bgColor
+    }
+
+    return <button style={style}>
+        {props.title}
+    </button>
 }
 
 export const App = () => {
-    const video = {
-        title: 'Samurai way',
-        link: 'https://www.youtube.com/watch?v=gb7gMluAeao&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8',
-        description: 'Best free react-course'
-    }
-
-    return <YoutubeVideo video={video} />
+    return <div>
+        <CrazyButton title={'delete'} fontColor={'white'} bgColor={'red'}/>
+        <CrazyButton title={'add'} fontColor={'white'} bgColor={'green'}/>
+    </div>
 }
 
-ReactDOM.render(<App />,
+ReactDOM.render(<App/>,
     document.getElementById('root')
-);
+)
 
-// Что нужно написать вместо xxx yyy zzz, чтобы увидеть ожидаемый результат?
-//     Ответ дайте через пробел, пример: a={12} ccc={video.id} d={'hello'}
+//Что нужно написать вместо XXX YYY ZZZ? Ответ дайте через пробел
