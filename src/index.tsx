2f78c7330883ad7c5ce2598e187033387b1ab707
PlatonKynin <kininplaton@gmail.com>
2024-04-17 20:59:33 +0300

    01sprint-3week-3task

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,42 @@
-import React, {MouseEvent} from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Button() {
-    const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
-        // console.log(typeof e)
-        console.log((typeof e) === "")
-    }
-    return <button onClick={onClickHandler} >Click</button>
+type UserType = {
+    id: number
+    name: string
+    age: number
 }
 
+function User(props: UserType) {
+    return (
+        <li>User {props.name}: {props.age} y.o.</li>
+    )
+}
+
+function UsersList() {
+    const data: Array<UserType> = [
+        {id: 1, name: "Bob", age: 25},
+        {id: 2, name: "Alex", age: 28},
+        {id: 3, name: "Ann", age: 23},
+        {id: 4, name: "John", age: 30},
+    ]
+    const [users, setUsers] = useState<Array<UserType>>(data)
+    // Необходимо отрендерить список пользователей старше 25 лет:
+    const getOlderThen25Users = (u: UserType) => u.age > 25
+    const olderThen25Users = users.filter(getOlderThen25Users)
+    return (
+        <main>
+            <h4>User list:</h4>
+            <ul>
+                { olderThen25Users.map(u => <User key={u.id} {...u}/>)}
+            </ul>
+        </main>
+    )
+}
 
 ReactDOM.render(
-    <Button/>, document.getElementById('root')
+    <UsersList/>, document.getElementById('root')
 );
 
-// Что надо написать вместо ххх, чтобы в консоль вывело true?
+// Что надо написать вместо xxx, чтобы код работал?
