c187e69c34b8a551c2721bc371460645b5f18da0
PlatonKynin <kininplaton@gmail.com>
2024-04-17 20:30:05 +0300

    01sprint-2week-1task

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,24 @@
-{ } === { }
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-/*Каков результат выполнения этого выражения?*/
+function UsersList() {
+    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+    const getUser = (user: string) => <li>{user}</li>
+    const usersList = users.map(getUser)
+    console.log(Array.isArray(usersList))
+
+    return (
+        <main>
+            <h4>User list:</h4>
+            <ul>
+                {usersList}
+            </ul>
+        </main>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
+// Что вернёт выражение: Array.isArray(usersList)?
