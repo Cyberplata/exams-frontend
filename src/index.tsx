2c022811cb6f5ce488ed576bb126ca9ae32a8ecb
PlatonKynin <kininplaton@gmail.com>
2024-04-17 21:08:24 +0300

    01sprint-3week-9task

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,10 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+const user = {}
+const activity = user
+const student = {}
 
-type UserType = {
-    id: number
-    name: string
-    age: number
+
+if(activity === user) {
+    console.log ("Yo!!!")
 }
 
-function User(props: UserType) {
-    return (
-        <li>User {props.name}: {props.age} y.o.</li>
-    )
-}
-
-function UsersList() {
-    const data: Array<UserType> = [
-        {id: 1, name: "Bob", age: 25},
-        {id: 2, name: "Alex", age: 28},
-        {id: 3, name: "Ann", age: 23},
-        {id: 4, name: "John", age: 30},
-    ]
-    const [users, setUsers] = useState<Array<UserType>>(data)
-    // Необходимо отрендерить список ользователей старше 25 лет:
-    const getOlderThen25Users = (u: UserType) => u.age > 25
-    const olderThen25Users = users.filter(getOlderThen25Users)
-    console.log(Array.isArray(olderThen25Users)) //true
-
-    return (
-        <main>
-            <h4>User list:</h4>
-            <ul>
-                { olderThen25Users.map(u => <User key={u.id} {...u}/>)}
-            </ul>
-        </main>
-    )
-}
-
-ReactDOM.render(
-    <UsersList/>, document.getElementById('root')
-);
-// Что вернёт выражение: Array.isArray(olderThen25Users)
+/*Какую переменную надо указать вместо XXX, кроме user, что бы в консоль вывелась строка "Yo!!!?*/
