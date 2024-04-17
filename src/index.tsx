18964764b2bb5ba06d33320359947ce42a393ef2
PlatonKynin <kininplaton@gmail.com>
2024-04-17 20:56:11 +0300

    01sprint-3week-2task

--- src/index.tsx
+++ src/index.tsx
@@ -1,54 +1,18 @@
-import React, {useState} from 'react';
+import React, {MouseEvent} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-type UserType = {
-    id: number
-    name: string
-    age: number
-}
-
-type UserPropsType = UserType & {
-    deleteUser: (id: number) => void
-}
-
-function User(props: UserPropsType) {
-    const deleteUser = () => props.deleteUser(props.id)
-    return (
-        <li>
-            <button onClick={deleteUser}>x</button>
-            User {props.name}: {props.age} y.o.
-        </li>
-    )
-}
-
-function UsersList() {
-    const data: Array<UserType> = [
-        {id: 1, name: "Bob", age: 25},
-        {id: 2, name: "Alex", age: 28},
-        {id: 3, name: "Ann", age: 23},
-        {id: 4, name: "John", age: 30},
-    ]
-    const [users, setUsers] = useState<Array<UserType>>(data)
-    const deleteUser = (userID: number) => {
-        const filteredUsers = users.filter(u => u.id !== userID)
-        setUsers(filteredUsers)
+function Button() {
+    const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
+        // console.log(typeof e)
+        console.log((typeof e) === "")
     }
-    return (
-        <main>
-            <h4>User list:</h4>
-            <ul>
-                {users.map(u => <User
-                    key={u.id}
-                    {...u}
-                    deleteUser={deleteUser}
-                />)}
-            </ul>
-        </main>
-    )
+    return <button onClick={onClickHandler} >Click</button>
 }
+
 
 ReactDOM.render(
-    <UsersList/>, document.getElementById('root')
+    <Button/>, document.getElementById('root')
 );
-// Что надо написать вместо xxx, чтобы код работал?
+
+// Что надо написать вместо ххх, чтобы в консоль вывело true?
