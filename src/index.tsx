8dd2c4459106daf318ddebb31f763f6e4633c158
PlatonKynin <kininplaton@gmail.com>
2024-04-17 21:01:15 +0300

    01sprint-3week-4task

--- src/index.tsx
+++ src/index.tsx
@@ -10,26 +10,23 @@ type UserType = {
 
 function User(props: UserType) {
     return (
-        <li>User {props.name}: {props.age} y.o.</li>
+        <li>Student {props.name}: {props.age} y.o.</li>
     )
 }
 
 function UsersList() {
     const data: Array<UserType> = [
-        {id: 1, name: "Bob", age: 25},
-        {id: 2, name: "Alex", age: 28},
-        {id: 3, name: "Ann", age: 23},
-        {id: 4, name: "John", age: 30},
+        {id: 1, name: "Bob", age: 34},
+        {id: 2, name: "Alex", age: 29},
+        {id: 3, name: "Ann", age: 25},
+        {id: 4, name: "John", age: 36},
     ]
     const [users, setUsers] = useState<Array<UserType>>(data)
-    // Необходимо отрендерить список пользователей старше 25 лет:
-    const getOlderThen25Users = (u: UserType) => u.age > 25
-    const olderThen25Users = users.filter(getOlderThen25Users)
     return (
         <main>
             <h4>User list:</h4>
             <ul>
-                { olderThen25Users.map(u => <User key={u.id} {...u}/>)}
+                { users.map(u => <User key={u.id} {...u} />) }
             </ul>
         </main>
     )
@@ -38,5 +35,4 @@ function UsersList() {
 ReactDOM.render(
     <UsersList/>, document.getElementById('root')
 );
-
 // Что надо написать вместо xxx, чтобы код работал?
