5f614099c70e62588a9f72174a4b883f3bf363af
PlatonKynin <kininplaton@gmail.com>
2024-04-24 22:00:43 +0300

    01sprint-total-9task

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,27 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-function UsersList() {
-    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
-    console.log(typeof setUsers) // function
-    return (
-        <p>Тут будет список пользователей</p>
-    )
+type UserWalletType = {
+    title: string
+    amount: number
+}
+type UserWalletPropsType = {
+    wallet: UserWalletType
 }
 
-ReactDOM.render(
-    <UsersList/>, document.getElementById('root')
-);
-// Что вернёт выражение: typeof setUsers?
+export const UserWallet: React.FC<UserWalletPropsType> = (props) => {
+    return <div>title: {props.wallet.title}, amount: {props.wallet.amount}</div>
+}
 
+export const UserMoney = () => {
+    const wallets = [
+        {title: 'bitcoin', amount: 1},
+        {title: '$', amount: 100}
+    ]
+
+    return <div>
+        <UserWallet wallet={props.wallet} />
+        <UserWallet wallet={wallets} />
+    </div>
+}
+
+// Что нужно написать вместо xxx и yyy, чтобы компонент UserMoney отобразил информацию о двух кошельках (bitcoin и $)?
+//
+// Ответ дайте через пробел, например: props.x wallets
