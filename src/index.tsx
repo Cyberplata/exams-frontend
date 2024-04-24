37edb75126a3b32d91f542ad257592405ea8e068
PlatonKynin <kininplaton@gmail.com>
2024-04-24 21:47:01 +0300

    01sprint-total-4task

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,47 @@
-import React, {useState} from 'react';
+import React, {useState, MouseEvent, ChangeEvent} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function UsersList() {
-    const results = useState<Array<string>>(["Bob", "Alex", "Ann"])
-    console.log(results.length)
-
-    const users = results[0]
-    const setUsers = results[1]
-
+function Notes() {
+    const [newNote, setNewNote] = useState<string>("")
+    const [notes, setNotes] = useState<Array<string>>([])
+    const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement> )=>
+        setNewNote(e.currentTarget.value)
+    const addNote = () => {
+        setNotes([newNote, ...notes])
+        setNewNote("")
+    }
     return (
-        <p>Тут будет список пользователей</p>
+        <div>
+            <textarea
+                value={newNote}
+                onChange={onChangeHandler}
+                onBlur={addNote}
+            />
+            <div>
+                <button
+                    onClick={()=>setNotes([''])}
+                >Clear notes list</button>
+            </div>
+            <h4>Notes:</h4>
+            <div>
+                {notes.map(n => <p>{n}</p>)}
+            </div>
+        </div>
     )
 }
 
 ReactDOM.render(
-    <UsersList/>, document.getElementById('root')
+    <Notes/>, document.getElementById('root')
 );
+// Что надо написать вместо ххх,
+// чтобы при клике список заметок очищался?
 
-// Чему равно results.length?
+
+
+
+
+
+
+
+
