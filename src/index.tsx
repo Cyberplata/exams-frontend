e21b1495d091e3fb6b1149408223fc092644d99d
PlatonKynin <kininplaton@gmail.com>
2024-04-24 21:51:10 +0300

    01sprint-total-6task

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,40 @@
-import ReactDOM from 'react-dom'
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-const Son = (props: any) => {
-    return <div>
-        I am son. My name is {props.name}
-    </div>
+type UserType = {
+    id: number
+    name: string
+    age: number
 }
 
-
-const Father = (props: any) => {
-    return <div>
-        I am father. My name is {props.name}
-        <Son name={props.sonName} />
-    </div>
+function User(props: UserType) {
+    return (
+        <li>User {props.name}: {props.age} y.o.</li>
+    )
 }
 
-const Granny = (props: any) => {
-    return <div>
-        I am granny. My name is {props.name}
-        <Father name={props.fatherName} sonName={props.sonName} />
-    </div>
+function UsersList() {
+    const data: Array<UserType> = [
+        {id: 1, name: "Bob", age: 25},
+        {id: 2, name: "Alex", age: 28},
+        {id: 3, name: "Ann", age: 23},
+        {id: 4, name: "John", age: 30},
+    ]
+    const [users, setUsers] = useState<Array<UserType>>(data)
+    // Пользователи старше 25 лет:
+    const olderThen25Users = users.filter(u => u.age > 25)
+    return (
+        <main>
+            <h4>User list:</h4>
+            <ul>
+                { olderThen25Users.map(u => <User key={u.id} {...u}/>)}
+            </ul>
+        </main>
+    )
 }
 
-export const App = () => {
-    return <div>
-        <Granny Granny={'Бабуля'} Father={'Батя'} Son={'Сын'}/>
-    </div>
-}
-
-ReactDOM.render(<App/>,
-    document.getElementById('root')
-)
-
-//Что нужно написать вместо XXX YYY ZZZ? Ответ дайте через пробел?
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
+// Что надо написать вместо xxx, чтобы код работал?
