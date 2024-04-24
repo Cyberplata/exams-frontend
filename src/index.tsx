bf9565f10500e616a8c318110943b5df05020f0e
PlatonKynin <kininplaton@gmail.com>
2024-04-24 21:40:33 +0300

    01sprint-4week-10task-correct answer

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,7 @@ function Notes() {
             />
             <div>
                 <button
-                    onClick={addNote}
+                    onClick={() => setNotes([])}
                 >Clear notes list</button>
             </div>
             <h4>Notes:</h4>
