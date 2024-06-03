41c0a619b35165d137f62a23335b1269df529124
PlatonKynin <kininplaton@gmail.com>
2024-06-03 16:32:15 +0300

    02sprint-1week-1

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
-type UserPropsType = {
-    name: string
-    description: string
-}
-export const User: React.FC<UserPropsType> = (props) => {
-    return <div>
-        <h1>Имя: {props.name}</h1>
-        <div>Описание: {props.description}</div>
-    </div>
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function PasswordChecker() {
+    const [password, setPassword] = useState<string>("")
+    return (
+        <main>
+            <p>Your password must have more than 8 charters!</p>
+            <input
+                placeholder={"Enter your password"}
+                value={password}
+                onChange={e => setPassword(e.currentTarget.value)}
+                type={"password"}
+            />
+            {password.length < 9 && <p style={{color: "red"}}>The password is too short!</p>}
+        </main>
+    )
 }
 
-//Что нужно написать вместо ххх, что бы код работал?
+ReactDOM.render(
+    <PasswordChecker/>, document.getElementById('root')
+);
+
+// Что надо вставить вместо XXX, чтобы код работал нормально?
