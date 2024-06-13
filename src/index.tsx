64fdf7279b02322351d3f711ce180197420618b3
PlatonKynin <kininplaton@gmail.com>
2024-06-13 20:01:33 +0300

    02sprint-2week-7

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,14 @@
-type StudentType = {
-    id: number
-    name: string
-    age: number
+const customer = {
+    name: "Maria",
+    age: 78,
+    friends: ["Rita", "Natali", "Konstantin"]
 }
 
-type FriendsType = {
-    [key: string]: Array<string>
+const updatedCustomer = {
+    ...customer,
+    friends: customer.friends
 }
 
+console.log(customer.friends === updatedCustomer.friends) //true
 
-export const students: Array<StudentType> = [
-    {id: 1, name: "Bob", age: 34},
-    {id: 2, name: "Alex", age: 23},
-    {id: 3, name: "Ann", age: 25},
-    {id: 4, name: "Charley", age: 20},
-]
-
-export const friends: FriendsType = {
-    1: ["Jack","Oliver", "Oscar",],
-    2: ["Jack", "Lewis", "Thomas",],
-    3: ["William",  "Lewis","Michael"],
-    4: ["Oscar", "Thomas", "William",],
-}
-console.log(students[1])
-console.log(students[3]) //да имею "Thomas"
-
-
-//Дан массив студентов и структура,
-//которая содержит список друзей каждого из студентов.
-//Id студента является ключом к массиву его друзей.
-//Имеют ли студенты students[1] и students[3] общих друзей?
-//Если да, напишите в ответе имя общего друга в кавычках, если нет - напишите в ответе false.
-
+/*Какое значение будет выведено в консоль?*/
