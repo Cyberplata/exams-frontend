cd3d94ed6a60385e317b8546d0a1d42e49569629
PlatonKynin <kininplaton@gmail.com>
2024-06-24 19:59:41 +0300

    02sprint-3week-10

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,20 @@
-import React, {useState} from "react";
-
-type UserType = {
-    id: number
-    name: string
-    status: "online" | "offline"
+const client = {
+    name: "Egor",
+    age: 98,
+    scores: [72.17, 18.29, 71.90]
 }
 
-type AddressType = {
-    country: string
-    city: string
-    email: string
+const [first, second, third = 35.45] = client.scores
+
+switch(third){
+    case 35.45:
+        console.log("Nauru")
+        break;
+    case 18.29:
+        console.log("Norway")
+        break;
+    default:
+        console.log("Barbuda"); // "Barbuda"
 }
+/*Какую строку мы увидим в консоли?*/
 
-type AdressesType = {
-    [userID: string]: AddressType
-}
-
-const users: Array<UserType> = [
-    {id: 1, name: "Bob", status: "online"},
-    {id: 2, name: "Alex", status: "offline"},
-    {id: 3, name: "Ann", status: "offline"},
-]
-
-const addresses: AdressesType = {
-    1: {country: "Russia", city: "Moscow", email: "hey@email.com"},
-    2: {country: "Ukraine", city: "Kiev", email: "yo@send.ua"},
-    3: {country: "Belarus", city: "Minsk", email: "wow@gogo.ru"},
-
-}
-
-const updateUserAddress = (userID: number, key: string, newValue: string) => {
-    return {...addresses,
-        [userID]: {...addresses[userID], [key]: newValue}
-    }
-}
-// Дан список пользователей и структура, хранящая адреса пользователей.
-// Так же дана функция updateUserAddress,
-// которая обновляет указанное в параметрах поле в адресе пользователя.
-// Пример использования функции: updateUserAddress(2, "city", "Dnepropetrovsk")
-// Что надо написать вместо ххх, чтобы функция работала корректно?
