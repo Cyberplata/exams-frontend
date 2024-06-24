52880a8334e9ca8f3b0887049ec5d658f42ac93a
PlatonKynin <kininplaton@gmail.com>
2024-06-24 19:56:14 +0300

    02sprint-3week-9

--- src/index.tsx
+++ src/index.tsx
@@ -16,23 +16,26 @@ type AdressesType = {
     [userID: string]: AddressType
 }
 
-
 const users: Array<UserType> = [
     {id: 1, name: "Bob", status: "online"},
     {id: 2, name: "Alex", status: "offline"},
     {id: 3, name: "Ann", status: "offline"},
 ]
 
-export const addresses: AdressesType = {
+const addresses: AdressesType = {
     1: {country: "Russia", city: "Moscow", email: "hey@email.com"},
     2: {country: "Ukraine", city: "Kiev", email: "yo@send.ua"},
     3: {country: "Belarus", city: "Minsk", email: "wow@gogo.ru"},
 
 }
 
-export const changeUserStatus = (userID: number, status: string) => {
-    return users.map(u => u.id === userID ? {...u, status} : u)
+const updateUserAddress = (userID: number, key: string, newValue: string) => {
+    return {...addresses,
+        [userID]: {...addresses[userID], [key]: newValue}
+    }
 }
 // Дан список пользователей и структура, хранящая адреса пользователей.
-// Так же дана функция changeUserStatus, которая меняет статус пользователя.
+// Так же дана функция updateUserAddress,
+// которая обновляет указанное в параметрах поле в адресе пользователя.
+// Пример использования функции: updateUserAddress(2, "city", "Dnepropetrovsk")
 // Что надо написать вместо ххх, чтобы функция работала корректно?
