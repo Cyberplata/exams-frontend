3e8a6edffa5b904bd13cf5390f0d185e694de8f8
PlatonKynin <kininplaton@gmail.com>
2024-06-24 19:46:57 +0300

    02sprint-3week-5

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,13 @@
-type StudentType = {
-    id: number
-    name: string
-}
+const names = ["Leonid", "Ivan", "Sergey"]
 
-type FriendsType = {
-    [key: string]: Array<string>
-}
+const nameNext = names
+// console.log(names[0])
+nameNext.length = 0
 
-export const students: Array<StudentType> = [
-    {id: 1, name: "Bob"},
-    {id: 2, name: "Alex"},
-    {id: 3, name: "Ann"},
-    {id: 4, name: "Charley"},
-]
+const result = names[0] ? 77 : 62
+console.log(result) // 62
 
-export const friends: FriendsType = {
-    1: ["Oliver", "Jack", "Oscar",],
-    2: ["Jack", "Lewis", "Thomas",],
-    3: ["William", "Michael", "Lewis",],
-    4: ["Oscar", "James", "William",],
-}
+/* Какое значение получит переменная result? */
 
-const getMutualFriends = (st_1: StudentType, st_2: StudentType,) => {
-    const result: Array<string> = []
-    friends[st_1.id].forEach(f => friends[st_2.id].includes(f) ? result.push(f) : null)
-    return result
-}
 
-//Дан массив студентов и структура,
-//которая содержит список друзей каждого из студентов.
-// Так же дана функция  getMutualFriends, проверяющая наличие общих друзей
-// у двух выбранных студентов.
-//Функция принимает параметром два объекта типа StudentType
-// и возвращает массив с именами общих друзей,
-//если они есть и пустой массив, если таковых нету.
-// Что надо написать вместо ххх, чтобы функция работала?
+
