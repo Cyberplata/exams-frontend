c5ba8b0a0d569a430df4d3df8bdfac188a62416f
PlatonKynin <kininplaton@gmail.com>
2024-07-16 21:19:05 +0300

    02sprint-4week-4

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 
 type ActionType = {
-    type: "SUM"|"SUB"|"MULT"|"DIV"
+    type: "SUM"|"SUB"|"MULT"|"DIV"|"EXP"
     payload: number
 }
 
-export const calculator = (state: number, action: ActionType) => {
+export const calculator = (state: number, action: ActionType): number => {
     switch (action.type) {
         case "SUM":
             return state + action.payload
@@ -12,9 +12,17 @@ export const calculator = (state: number, action: ActionType) => {
             return state - action.payload
         case "DIV":
             return state / action.payload
+        case "MULT":
+            return state * action.payload
+        case "EXP":
+            return state ** action.payload
         default:
             return state
     }
 }
+const result = calculator(10, {type: "EXP", payload: 0})
+if (!(result - 1)){
+    console.log("IT-INCUBATOR")
+}
 
-//Что надо написать вместо ХХХ, чтобы функция calculator работала? // action.type
+//Что надо написать вместо XXX чтобы в консоли появилась строка "IT-INCUBATOR"? //type: "EXP"
