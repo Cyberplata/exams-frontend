e21533f654c46e81976f4914a90404110e7e753a
PlatonKynin <kininplaton@gmail.com>
2024-07-16 21:28:21 +0300

    02sprint-4week-8

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 
 type ActionType = {
-    type: "SUM"|"SUB"|"MULT"|"DIV"|"EXP"
+    type: "SUM"|"SUB"|"MULT"|"DIV"
     payload: number
 }
 
@@ -12,11 +12,13 @@ export const calculator = (state: number, action: ActionType): number => {
             return state - action.payload
         case "DIV":
             return state / action.payload
-        case "EXP":
-            return state ** action.payload
+        case "MULT":
+            return state * action.payload
         default:
             return state
     }
 }
 
-//Что вернёт такой вызов функции: calculator(10, {type: "MULT", payload: 2})? // 10
+const result = calculator(10, {type: "SUB", payload: 5})
+console.log(result)
+//Что надо написать вместо XXX, что бы переменная result содержала значение 5? //type: "SUB"
