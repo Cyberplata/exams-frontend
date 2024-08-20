944237e2c23988a0adf046acfccc3d843c1d6dc9
PlatonKynin <kininplaton@gmail.com>
2024-08-20 20:45:17 +0300

    03sprint-1week-3

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,34 @@
-const array = [1,2,3,4,5]
-const result = array.push(6) - array.pop()
+type Status = 'Stopped' | 'Playing' | 'Paused'
+type StateType = {
+    volume: number // in percents
+    trackUrl: string // 'https://blabla.com/track01.mp3',
+    currentPlayPosition: number // milliseconds,
+    status: Status
+}
+export const playerReducer = (state: StateType, action: any) => {
+    switch (action.type) {
+        case 'TRACK-VOLUME-CHANGED':
+            return {
+                ...state,
+                volume: action.volumeLevel
+            }
+        default:
+            return state
+    }
+}
 
-const bestStudent = result
-    ? "Sergey"
-    : "Artur"
+const muteTrackAC = () => ({type: 'TRACK-MUTED'})
+const changeVolumeAC = (volumeLevel: number) => ({type: 'TRACK-VOLUME-CHANGED', volumeLevel})
+const changeTrackAC = (url: string) => ({type: 'TRACK-URL-CHANGED', url})
+const changeTrackPlayStatusAC = (status: Status) => ({type: 'TRACK-STATUS-CHANGED', status})
 
-//Какое значение получит переменная bestStudent? "Artur"
+const state: StateType = {
+    status: 'Stopped',
+    currentPlayPosition: 1213,
+    trackUrl: 'https://blabla.com/track01.mp3',
+    volume: 100
+}
+const newState = playerReducer(state, changeVolumeAC(20))
+console.log(newState.volume === 20)
+
+// Напишите вместо XXX правильную строку кода, чтобы изменить громкость трека и увидеть в консоли true. // volume: action.volumeLevel
