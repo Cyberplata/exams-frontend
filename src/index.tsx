bd8e9f2a641342b5073724dbb481c044718582ee
PlatonKynin <kininplaton@gmail.com>
2024-08-20 20:50:52 +0300

    03sprint-1week-5

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,17 @@
-type StateType = {
-    volume: number // in percents
-    trackUrl: string // 'https://blabla.com/track01.mp3',
-    currentPlayPosition: number // milliseconds,
+//@ts-ignore
+const value = [0,[1,0]][1][0]
+let quantity;
+
+switch (value) {
+    case 0:
+        quantity = 87
+        break;
+    case 1:
+        quantity = 66
+        break;
+    default:
+        quantity = 6
+        break;
 }
 
-export const reducer = (state: StateType, action: any) => {
-    switch (action.type) {
-        case 'TRACK-URL-CHANGED':
-            return {
-                ...state,
-                trackUrl: action.url
-            }
-        case 'TRACK-MUTED':
-            return {
-                ...state,
-                volume: 0
-            }
-        case 'TRACK-REWOUND-TO-START':
-            return {
-                ...state,
-                currentPlayPosition: 0
-            }
-        default:
-            return state
-    }
-}
-
-const muteTrackAC = () => ({type: 'TRACK-MUTED'})
-const changeTrackAC = (url: string) => ({type: 'TRACK-URL-CHANGED', url})
-// перемотатьНаНачало:
-const rewindToStart = () => ({type: 'TRACK-REWOUND-TO-START'})
-
-// Какие типы должны быть вместо XXX, YYY и ZZZ?
-// Ответ дать через пробел, например:   'BLABLA' 'HEYНЕY' 'HIPHOP' // 'TRACK-URL-CHANGED' 'TRACK-MUTED' 'TRACK-REWOUND-TO-START'
+// Какое значение получит переменная quantity? // 66
