7abe1ad74df20838a1a956ae0a4b21496c4c9ead
PlatonKynin <kininplaton@gmail.com>
2024-08-20 20:55:54 +0300

    03sprint-1week-6

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
-//@ts-ignore
-const value = [0,[1,0]][1][0]
-let quantity;
-
-switch (value) {
-    case 0:
-        quantity = 87
-        break;
-    case 1:
-        quantity = 66
-        break;
-    default:
-        quantity = 6
-        break;
+export const reducer = (state: any, action: any) => {
+    switch (action.type) {
+        case 'TRACK-ADDED':
+            return [...state, {id: action.trackId, likesCount: 0}]
+        default:
+            return state
+    }
 }
 
-// Какое значение получит переменная quantity? // 66
+const addTrackAC = (trackId: number) => ({type: 'TRACK-ADDED', trackId})
+
+
+const state = [
+    {id: 12, likesCount: 10},
+    {id: 14, likesCount: 2},
+    {id: 100, likesCount: 0}
+]
+const newState = reducer(state, addTrackAC(300))
+
+console.log(newState[3].likesCount === 0)
+
+// Что нужно написать вместо XXX, чтобы трек корректно добавился и в консоли увидеть true? // [...state, {id: action.trackId, likesCount: 0}]
