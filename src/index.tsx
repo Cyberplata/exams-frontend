3415a950da0c8f703ec7295be854f424cbd5aefe
PlatonKynin <kininplaton@gmail.com>
2024-11-12 21:54:26 +0300

    03sprint-4week-5

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-const students = [
-    {name: "Bob"},
-    {name: "Alex"},
-    {name: "Donald"}
-]
-const filteredStudents = students.filter(s => s.name !== "Kate")
-const lengthDiff = students.length - filteredStudents.length
-const newValue = (lengthDiff && 70.97) || (52.36 && 69.16)
+const value = 32
 
-//Какое значение получит переменная newValue? 69.16
+const getValue = (value: any) => {
+    value += 49
+    return value
+}
+
+const myResult= getValue("") || value
+
+//Какое значение получит переменная myResult? "49"
