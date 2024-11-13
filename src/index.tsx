d01be2942976d42da5dec10b4b15049d4978c5ba
PlatonKynin <kininplaton@gmail.com>
2024-11-13 20:43:21 +0300

    03sprint-total-8

--- src/index.tsx
+++ src/index.tsx
@@ -1,67 +1,23 @@
-import React from 'react'
-import { createStore } from 'redux'
-import { Provider, useSelector, useDispatch } from 'react-redux'
-import ReactDOM from 'react-dom'
-
-type StudentType = {
-    id: number
-    name: string
-    age: number
-}
-
-const initState = {
-    students:
-        [
-            {id: 1, name: 'Bob', age: 23},
-            {id: 2, name: 'Alex', age: 22}
-        ] as Array<StudentType>
-}
-type AddStudentAT = {
-    type: 'ADD-STUDENT'
-    name: string
-    age: number
-    id: number
-}
-
-type InitialStateType = typeof initState
-
-const studentsReducer = (state: InitialStateType = initState, action: AddStudentAT): InitialStateType => {
+export const reducer = (state: any, action: any) => {
     switch (action.type) {
-        case 'ADD-STUDENT':
-            return {
-                ...state,
-                students: [...state.students, {
-                    name: action.name,
-                    age: action.age,
-                    id: action.id
-                }]
-            }
+        case 'TRACK-DELETED':
+            return state.filter((track: any) => track.id !== action.trackId)
+        default:
+            return state
     }
-    return state
 }
 
-const appStore = createStore(studentsReducer)
-type RootStateType = ReturnType<typeof studentsReducer>
+const deleteTrackAC = (trackId: number) => ({type: 'TRACK-DELETED', trackId})
 
 
-const StudentList = () => {
-    const students = useSelector((state: RootStateType) => state.students)
-    return (
-        <ul>
-            {students.map(s => <li key={s.id}>{`${s.name}. ${s.age} years.`}</li>)}
-        </ul>
-    )
-}
-const App = () => {
-    return <StudentList/>
-}
+const state = [
+    {id: 12, likesCount: 10},
+    {id: 14, likesCount: 2},
+    {id: 100, likesCount: 0}
+]
+const newState = reducer(state, deleteTrackAC(14))
 
-ReactDOM.render(<div>
-        <Provider store={appStore}>
-            <App/>
-        </Provider>
-    </div>,
-    document.getElementById('root')
-)
+console.log(newState.length === 2)
 
-// Что нужно написать вместо XXX, YYY и ZZZ, чтобы отобразился список студентов? // Provider store appStore
+
+// Что нужно написать вместо XXX, чтобы корректно удалить трек и в консоли увидеть true? // track.id !== action.trackId
