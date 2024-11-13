65636384b24f2b129c348273ededc7bd02e37ff3
PlatonKynin <kininplaton@gmail.com>
2024-11-13 20:38:56 +0300

    03sprint-total-6

--- src/index.tsx
+++ src/index.tsx
@@ -1,66 +1,45 @@
-import React, {useState, useReducer, useEffect} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-const changeCounter = (state: number, action: any): number => {
+export const reducer = (state: any, action: any) => {
     switch (action.type) {
-        case "INC_VALUE":
-            return state + 1
-        case "RESET":
-            return 0
-        case "DEC_VALUE":
-            return state - 1
+        case 'USER-NAME-UPDATED':
+            return {
+                ...state,
+                user: {
+                    ...state.user,
+                    name: action.name
+                }
+            }
+
         default:
             return state
     }
 }
 
-function Counter() {
-    const [value, setValue] = useReducer(changeCounter, 0)
-    const [isCounter, setIsCounter] = useState(true)
-    const commonStyles: React.CSSProperties = {
-        border: "1px solid black",
-        margin: "100px auto",
-        width: "300px",
-        height: "150px",
-        textAlign: "center",
-    }
-    const btnStyles: React.CSSProperties = {
-        color: "white",
-        fontWeight: "bold",
-        backgroundColor: "darkgray",
-        borderRadius: "3px",
-        minWidth: "40px"
-    }
+const updateUserNameAC = (name: string) => ({type: 'USER-NAME-UPDATED', name})
 
-    return (
-        <div style={commonStyles}>{
-            isCounter
-                ? <div >
-                    <div style={{marginBottom: "20px"}}>
-                        <h2>{value}</h2>
-                        <button
-                            style={{...btnStyles, backgroundColor: "red"}}
-                            onClick={() => setIsCounter(false)}>OFF</button>
-                    </div>
-                    <button style={btnStyles} onClick={() => setValue({type: "INC_VALUE"})}>+</button>
-                    <button style={btnStyles} onClick={() => setValue({type: "RESET"})}>0</button>
-                    <button style={btnStyles} onClick={() => setValue({type: "DEC_VALUE"})}>-</button>
 
-                </div>
-                : <div style={{textAlign: "center"}}>
-                    <h2>Counter not working</h2>
-                    <button
-                        style={{...btnStyles, backgroundColor: "green"}}
-                        onClick={() => setIsCounter(true)}>ON</button>
-                </div>
-        }
-        </div>
-    )
+const state = {
+    count: 10,
+    user: {
+        name: 'Dimych',
+        age: 18,
+        isMarried: true,
+        status: "offline"
+    },
+    books: ['you don\'t know JS']
 }
+const newState = reducer(state, updateUserNameAC('Dmitry'))
 
+console.log(newState.user.name === 'Dmitry')
+console.log(newState.books === state.books)
+console.log(newState.user !== state.user)
 
-ReactDOM.render(
-    <Counter/>, document.getElementById('root')
-);
-// Что надо написать вместо XXX и YYY, чтобы код работал? Напишите через пробел. // useReducer useState
+//Что нужно написать вместо XXX, чтобы корректно обновить имя пользователя и в консоли увидеть:  true true true?
+/*
+* {
+                ...state,
+                user: {
+                    ...state.user,
+                    name: action.name
+                }
+            }
+* */
