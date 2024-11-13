543be5de9728fa4eb8b27c93c9fa6ab707469383
PlatonKynin <kininplaton@gmail.com>
2024-11-13 20:30:24 +0300

    03sprint-total-2

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,34 @@
-import {combineReducers, createStore} from 'redux'
-
-let initialState = {items: [{name: 'Dimych'}, {name: 'Ignat'}]}
-const usersReducer = (state = initialState, action: any) => {
-    return state
+type Status = 'Stopped' | 'Playing' | 'Paused'
+type StateType = {
+    volume: number // in percents
+    trackUrl: string // 'https://blabla.com/track01.mp3',
+    currentPlayPosition: number // milliseconds,
+    status: Status
+}
+export const playerReducer = (state: StateType, action: any) => {
+    switch (action.type) {
+        case 'TRACK-VOLUME-CHANGED':
+            return {
+                ...state,
+                volume: action.volumeLevel
+            }
+        default:
+            return state
+    }
 }
 
-const store = createStore(combineReducers({
-    users: usersReducer
-}))
+const muteTrackAC = () => ({type: 'TRACK-MUTED'})
+const changeVolumeAC = (volumeLevel: number) => ({type: 'TRACK-VOLUME-CHANGED', volumeLevel})
+const changeTrackAC = (url: string) => ({type: 'TRACK-URL-CHANGED', url})
+const changeTrackPlayStatusAC = (status: Status) => ({type: 'TRACK-STATUS-CHANGED', status})
 
-store.subscribe(() => {
-    console.log('state changed')
-})
+const state: StateType = {
+    status: 'Stopped',
+    currentPlayPosition: 1213,
+    trackUrl: 'https://blabla.com/track01.mp3',
+    volume: 100
+}
+const newState = playerReducer(state, changeVolumeAC(20))
+console.log(newState.volume === 20)
 
-store.dispatch({type: 'ANY'})
-
-// Что нужно написать вместо XXX, чтобы в консоли увидеть 'state changed'? // subscribe
+// Напишите вместо XXX правильную строку кода, чтобы изменить громкость трека и увидеть в консоли true. // volume: action.volumeLevel
