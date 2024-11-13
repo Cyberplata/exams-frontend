9192a3975f008818a85cb500f2b118f2959e07f4
PlatonKynin <kininplaton@gmail.com>
2024-11-13 20:25:58 +0300

    03sprint-total-1

--- src/index.tsx
+++ src/index.tsx
@@ -5,22 +5,14 @@ const usersReducer = (state = initialState, action: any) => {
     return state
 }
 
-let authInitialState = {login: 'Margo', settings: {theme: 'dark'}}
-const authReducer = (state = authInitialState, action: any) => {
-    return state
-}
-
 const store = createStore(combineReducers({
-    users: usersReducer,
-    auth: authReducer
+    users: usersReducer
 }))
 
 store.subscribe(() => {
-    const login = store.getState().auth.login
-    console.log(login)
+    console.log('state changed')
 })
 
 store.dispatch({type: 'ANY'})
-export default store;
 
-// Что нужно написать вместо XXX, чтобы в консоли увидеть 'Margo'? auth: authReducer
+// Что нужно написать вместо XXX, чтобы в консоли увидеть 'state changed'? // subscribe
